563174413123d127f272e06d98fad152150345ba
Farkas Istvan <istvan.farkas@kronsoft.ro>
2018-02-02 14:28:56 +0200

    Commit start project

---
+++ src/main/java/ro/wolfnet/helloworld/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package ro.wolfnet.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
